570b46daac5c1ac1b66a629a04cf9a5805b0a75a
Fran1409 <fran_andries@hotmail.com>
2021-01-08 15:42:21 +0100

    Get the total value of ordered items

--- index.php
+++ index.php
@@ -10,7 +10,8 @@ declare(strict_types=1);
 session_start();
 
 // Use this function when you need to need an overview of these variables
-function whatIsHappening() {
+function whatIsHappening()
+{
     echo '<h2>$_GET</h2>';
     var_dump($_GET);
     echo '<h2>$_POST</h2>';
@@ -23,9 +24,29 @@ function whatIsHappening() {
 
 // TODO: provide some products (you may overwrite the example)
 $products = [
-    ['name' => 'Your favourite drink', 'price' => 2.5],
+    ['name' => 'Lazy Red Cheeks', 'price' => 10],
+    ['name' => 'Pornstar Martini', 'price' => 12],
+    ['name' => 'Cosmopolitan', 'price' => 11],
+    ['name' => 'Margarita', 'price' => 12],
+    ['name' => 'Long Island Ice Tea', 'price' => 12],
+    ['name' => 'Bart', 'price' => 10],
 ];
 
 $totalValue = 0;
+
+
+if (isset($_POST['submit'])) {
+    $amount = 0;
+    $listProducts = $_POST['products'];
+    //var_dump($_POST['products']);
+
+    foreach ($listProducts as $selected => $product) {
+        $totalValue = $totalValue + $products[$product]['price'];
+        $amount++;
+        
+    }
+
+}
+
 
 require 'form-view.php';
